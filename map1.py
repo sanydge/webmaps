34971d19b216ac665b1475234153657312f0ebc8
sanydge <sanydge@gmail.com>
2020-03-19 12:44:51 +0200

    adding colors by population

--- map1.py
+++ map1.py
@@ -22,14 +22,17 @@ map = folium.Map(location=[44.421054, -117.814664], zoom_start=6, tiles="Stamen 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(str(el)+"m", parse_html=True),
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(str(el) + "m", parse_html=True),
                                icon=folium.Icon(color=color_producer(el))))
 
 fg2 = folium.FeatureGroup("Population")
-fg2.add_child(folium.GeoJson(data=open('world.json', encoding='utf-8-sig').read()))
+fg2.add_child(folium.GeoJson(data=open('world.json', encoding='utf-8-sig').read(),
+                             style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
+                             else 'orange' if 10000000 <= x['properties']['POP2005']
+                                              < 20000000 else 'red'}))
+
 map.add_child(fg2)
 
 map.add_child(fg)
 
 map.save("Map1.html")
-
